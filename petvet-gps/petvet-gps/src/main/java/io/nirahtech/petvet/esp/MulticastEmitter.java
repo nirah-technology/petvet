b18d5d78dff92c492b7367d1afe968e06fecd4c0
Nicolas METIVIER <nicolas.a.metivier@gmail.com>
2024-06-16 19:08:54 +0200

    Detach responsabilities

--- petvet-gps/petvet-gps/src/main/java/io/nirahtech/petvet/esp/MulticastEmitter.java
+++ petvet-gps/petvet-gps/src/main/java/io/nirahtech/petvet/esp/MulticastEmitter.java
@@ -3,19 +3,25 @@ package io.nirahtech.petvet.esp;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
+import java.net.NetworkInterface;
 import java.util.Objects;
 
 public final class MulticastEmitter {
+    public static final String WIFI_NETWORK_INTERFACE_NAME = "wlo1";
 
     private MulticastEmitter() { }
 
-    public static final void broadcast(final InetAddress group, final int port, final String message) throws IOException {
-        Objects.requireNonNull(group, "Group is required to send message.");
+    public static final void broadcast(final InetAddress multicastGroupAddress, final int port, final String message) throws IOException {
+        Objects.requireNonNull(multicastGroupAddress, "Group is required to send message.");
         Objects.requireNonNull(message, "Message is required to send message.");
-        final MulticastSocket multicastSocket = new MulticastSocket();
-        final DatagramPacket udpPacket = new DatagramPacket(message.getBytes(), message.length(), group, port);
-        multicastSocket.send(udpPacket);
-        multicastSocket.close();
+        InetSocketAddress group = new InetSocketAddress(multicastGroupAddress, port);
+        NetworkInterface networkInterface = NetworkInterface.getByName(WIFI_NETWORK_INTERFACE_NAME);
+        try (MulticastSocket multicastSocket = new MulticastSocket(group.getPort())) {
+            multicastSocket.joinGroup(new InetSocketAddress(multicastGroupAddress, 0), networkInterface);
+            final DatagramPacket udpPacket = new DatagramPacket(message.getBytes(), message.length(), group);
+            multicastSocket.send(udpPacket);
+        }
     }
 }
