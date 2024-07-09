74861220b2d227542a15b3a3701ca2d6deefb1ae
Nicolas METIVIER <nicolas.a.metivier@gmail.com>
2024-07-09 09:36:02 +0200

    Update panels

--- petvet-cluster-monitor/src/main/java/io/nirahtech/petvet/cluster/monitor/ui/features/reports/ScanReportsPanel.java
+++ petvet-cluster-monitor/src/main/java/io/nirahtech/petvet/cluster/monitor/ui/features/reports/ScanReportsPanel.java
@@ -1,13 +1,13 @@
 package io.nirahtech.petvet.cluster.monitor.ui.features.reports;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
 import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.UUID;
 
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 import io.nirahtech.petvet.cluster.monitor.MonitorTask;
 import io.nirahtech.petvet.cluster.monitor.data.ElectronicalCard;
@@ -28,13 +28,14 @@ public final class ScanReportsPanel extends JPanel {
 
     public ScanReportsPanel(SortedSet<ElectronicalCard> esps, Map<ScanNow, SortedSet<ScanReport>> scanReports, MonitorTask monitorTask) {
         super(new BorderLayout());
-        final JPanel container = new JPanel(new GridLayout(2, 1));
-
+        final JPanel container = new JPanel(new BorderLayout());
+        
+        
         this.scansReportsPointOfViewPanel = new ScansReportsPointOfViewPanel(esps, scanReports);
         this.scannerPointOfViewPanel = new ScannerPointOfViewPanel(esps, scanReports);
+        final JSplitPane splitePanel = new JSplitPane(JSplitPane.VERTICAL_SPLIT, this.scansReportsPointOfViewPanel, this.scannerPointOfViewPanel);
 
-        container.add(this.scansReportsPointOfViewPanel);
-        container.add(this.scannerPointOfViewPanel);
+        container.add(splitePanel);
 
         this.add(container, BorderLayout.CENTER);
 
@@ -42,7 +43,9 @@ public final class ScanReportsPanel extends JPanel {
             if (message instanceof ScanNowMessage) { 
                 ScanNowMessage realMessage = (ScanNowMessage) message;
                 final ScanNow scanNow = ScanNow.map(realMessage);
+                this.scansReportsPointOfViewPanel.setScanNow(scanNow);
                 scanReports.put(scanNow, new TreeSet<>());
+                this.scansReportsPointOfViewPanel.refresh();
             } else if (message instanceof ScanReportMessage) {
                 ScanReportMessage realMessage = (ScanReportMessage) message;
                 final ScanReport scanReport = ScanReport.map(realMessage);
